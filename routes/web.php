2393af4c503e3e9c4f5fb5156e838b6dff2bdcd1
Akash Kumar <akash.kumar@bwtech.co>
2021-10-08 06:05:43 +0500

    travel_list

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\DB;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/', function () {
+    $visited = DB::select('select * from places where visited = ?', [1]);
+    $togo = DB::select('select * from places where visited = ?', [0]);
+  
+    return view('travel_list', ['visited' => $visited, 'togo' => $togo ] );
+  });
