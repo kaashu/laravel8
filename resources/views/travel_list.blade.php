5d82143381d5a9f79d1f80540163c22a7f5a0ec1
Akash Kumar <akash.kumar@bwtech.co>
2021-10-08 06:07:50 +0500

    travel_list

--- resources/views/travel_list.blade.php
+++ resources/views/travel_list.blade.php
@@ -4,7 +4,8 @@
 </head>
 
 <body>
-    <h1>My Travel Bucket List</h1>
+    <h1>Laravel 8</h1>
+    <h2>My Travel Bucket List</h2>
     <h2>Places I'd Like to Visit</h2>
     <ul>
       @foreach ($togo as $newplace)
